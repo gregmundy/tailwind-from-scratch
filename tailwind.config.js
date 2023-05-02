b3b84400924e363c6104709d99ad0c26be86005d
Greg <gregmundy@gmail.com>
2023-05-01 22:55:12 -0400

    feat: complete fifth intro project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ['Mulish', 'sans-serif'],
+      mono: ['Rokkitt', 'monospace'],
+    },
     extend: {},
   },
   plugins: [],
